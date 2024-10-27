9052e8107b61065935d20f1e202f4220182226d7
21120457 <114399314+mihoag@users.noreply.github.com>
2024-10-27 13:53:48 +0700

    Complete full structure of this project

---
+++ PersonalFinanceApiService/src/main/java/com/hcmus/personalfinanceapiservice/PersonalFinanceApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hcmus.personalfinanceapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceApiServiceApplication.class, args);
+	}
+
+}
---
+++ PersonalFinanceApiService/src/test/java/com/hcmus/personalfinanceapiservice/PersonalFinanceApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmus.personalfinanceapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
